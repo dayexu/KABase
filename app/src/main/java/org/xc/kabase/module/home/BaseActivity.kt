8f174cf65f5119f70353560daadb33e6d5d70257
riguangx <xc_jy@sina.cn>
2018-11-08 17:36:21 +0800

    迭代

---
+++ app/src/main/java/org/xc/kabase/module/home/BaseActivity.kt
@@ -0,0 +1,9 @@
+package org.xc.kabase.module.home
+
+import android.support.v4.app.FragmentActivity
+
+/**
+ * Created by Administrator on 2018/11/8.
+ */
+class BaseActivity: FragmentActivity() {
+}
---
+++ app/src/main/java/org/xc/kabase/module/login/WelcomeActivity.kt
@@ -0,0 +1,17 @@
+package org.xc.kabase.module.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.xc.kabase.R
+
+/**
+ * 欢迎页
+ * Created by Administrator on 2018/11/8.
+ */
+class WelcomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_welcome)
+    }
+}
